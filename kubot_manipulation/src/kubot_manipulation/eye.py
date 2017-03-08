b666f7b5ae2ed68974264ab96ff007d0a3d88d36
Cem Eteke <ceteke13@ku.edu.tr>
2017-03-09 01:57:06 +0300

    Save before and after features

--- kubot_manipulation/src/kubot_manipulation/eye.py
+++ kubot_manipulation/src/kubot_manipulation/eye.py
@@ -3,6 +3,7 @@ from sensor_msgs.msg import PointCloud2
 import python_pcd
 import datetime
 import rosbag
+from pc_segmentation.msg import PcFeatures
 
 class Eye:
     def __init__(self, run_id):
@@ -10,17 +11,28 @@ class Eye:
         self.pcd_base_path = '/home/cem/ros_data/pcd/'
 
         self.features_topic = '/baris/features'
-        self.features_path = '/home/cem/ros_data/features/'
+        self.features_base_path = '/home/cem/ros_data/features/'
 
         self.run_id = run_id
+
+    def save_data(self,obj_name,iteration_num,status):
+        self.save_pcd(obj_name,iteration_num,status)
+        self.save_features(obj_name,iteration_num,status)
 
     def save_pcd(self,obj_name,iteration_num,status):
         msg = rospy.wait_for_message(self.pcd_topic, PointCloud2)
         python_pcd.write_pcd(self.pcd_base_path+self.generate_file_name(obj_name,iteration_num,status,True), msg)
 
-#    def save_features():
+    def save_features(self,obj_name,iteration_num,status):
+        msg = rospy.wait_for_message(self.features_topic, PcFeatures)
+        try:
+            bag = rosbag.Bag(self.features_base_path+self.generate_file_name(obj_name,iteration_num,status,False), 'w')
+            bag.write(self.features_topic,msg)
+        finally:
+            bag.close()
+
     def generate_file_name(self, obj_name, iteration_num, status, is_pcd):
         file_name = '%d_%d_%s_%s' % (self.run_id, iteration_num, obj_name, status)
         if is_pcd:
             return file_name + '.pcd'
-        return file_name
+        return file_name + '.bag'
--- kubot_manipulation/src/kubot_manipulation/tests.py
+++ kubot_manipulation/src/kubot_manipulation/tests.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 import rospy
 from arm import Arm
+import rosbag
 
 def main():
 
     rospy.init_node('testing_stuff', anonymous=True)
 
-    arm = Arm()
-    arm.ang_cmd([0.7,-1.5,2.1,-0.5,1.5,1.5])
-    rospy.spin()
+    bag = rosbag.Bag('/home/cem/ros_data/features/14_1_box2_before.bag')
+    for topic, msg, t in bag.read_messages(topics=['/baris/features']):
+        print msg
+    bag.close()
 
 if __name__ == '__main__':
     try:
